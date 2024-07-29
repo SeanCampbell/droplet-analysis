75fd7f1e577641cb842200792232e67482bc23c7
spcampbell08 <spcampbell08@gmail.com>
2024-07-29 13:18:50 +0000

    Install Python

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,3 +1,13 @@
 { pkgs, ... }: {
-services.docker.enable = true;
+    services.docker.enable = true;
+    packages = [
+        pkgs.python311
+        pkgs.python311Packages.pip
+    ];
+    idx = {
+        extensions = [
+            "ms-python.python"
+            "ms-python.debugpy"
+        ];
+    };
 }
